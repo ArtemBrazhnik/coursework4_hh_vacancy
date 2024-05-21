97a0777e3afe68a312c5830dc964ac9bd4c08548
Artem Brazhnik <dambldor55@gmail.com>
2024-05-21 16:22:53 +0300

    create vacancy.py

---
+++ src/vacancy.py
@@ -0,0 +1,28 @@
+class Vacancy:
+    """Информация о вакансии"""
+
+    title: str
+    url: str
+    salary: int
+    schedule: str
+
+    def __init__(self, title, url, salary, schedule):
+        self.title = title
+        self.url = url
+        self.salary = salary or 0
+        self.schedule = schedule
+
+    def __str__(self):
+        return f'{self.title} {self.salary} {self.schedule} {self.url}'
+
+    def __gt__(self, other):
+        return self.salary > other.salary
+
+    def to_json(self):
+        """Конвертирует вакансии в JSON"""
+        return {
+            'title': self.title,
+            'url': self.url,
+            'salary': self.salary,
+            'schedule': self.schedule,
+        }
